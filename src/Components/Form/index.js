f9009b93de58533e9ad149d4cff848ace69f1c64
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-18 18:33:22 -0300

    Adding filters to select only available times

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Api from "../../Services/api";
 import { Formik, ErrorMessage, Form as FormFormik, Field } from "formik";
 
 import schema from "./schema";
-import DatePickerForm from "../DatePickerForm";
+import DatePicker from "react-datepicker";
 
 import "./styles.css";
+import "react-datepicker/dist/react-datepicker.css";
 import TitleForm from "../../Assets/Title-form.svg";
 
 
 export default function Form({handleSubmit, initialValues}) {
+
+  const [hours, setHours] = useState([]);
+
+  const handleSelectedDate = async (dateSelect) => {
+    let day = "" + dateSelect.getDate();
+    let month = "" + (dateSelect.getMonth() + 1);
+    let year = dateSelect.getFullYear();
+
+    if(day.length < 2) day = "0" + day;
+    if(month.length < 2) month = "0" + month;
+
+    let date = [day, month, year].join("-");
+    let list = await Api.get(`/api/agendamento?dia=${date}`);
+    setHours(filterHours(list.data.attendanceTimes));
+  }
+
+
+  const filterHours = (list) => {
+    let listFilter = ["07:00", "08:00", "09:00", "10:00","11:00","12:00","14:00","15:00","16:00","17:00"];
+    if(!(list === undefined)){
+      list.map((hour) => {
+        if(listFilter.includes(hour)){
+          let count = list.filter(x => x === hour).length;
+          if(count === 2){
+            let index = listFilter.indexOf(hour);
+            listFilter.splice(index, 1);
+          }
+        }
+      });
+    }
+    return listFilter;
+  }
+
+  const getDateTomorrow = () => {
+    let currentDate = new Date();
+    let tomorrowDate = new Date();
+    tomorrowDate.setDate(currentDate.getDate() + 1)
+    return tomorrowDate;
+  }
   
   return (
     <Formik
@@ -27,21 +68,53 @@ export default function Form({handleSubmit, initialValues}) {
               <ErrorMessage className="message-error" component="span" name="name"/>
               <div className="content-input">
                 <p>Data de nascimento:</p>
-                <DatePickerForm name={"birthDate"} placeholder={"Selecione a data"} ageCalendar={true}/>
+                <Field name="birthDate">
+                  {({form, field}) => {
+                    const {setFieldValue} = form
+                    const { value } = field
+                    return (
+                      <DatePicker
+                        id="birthDate"
+                        selected={value}
+                        onChange={date => setFieldValue("birthDate", date)}
+                        placeholderText="Selecione a data"
+                        dateFormat="dd/MM/yyyy"
+                        />
+                    );
+                  }}
+                </Field>
               </div>
               <ErrorMessage className="message-error" component="span" name="birthDate" />
               <div className="content-input">
                 <p>Agendar vacina para o dia:</p>
-                <DatePickerForm name={"dateAppointment"} placeholder={"Selecione o dia"} ageCalendar={false}/>
+                <Field name="dateAppointment">
+                  {({form, field}) => {
+                    const {setFieldValue} = form
+                    const { value } = field
+                    return (
+                      <DatePicker
+                        id="dateAppointment"
+                        selected={value}
+                        onChange={date => setFieldValue("dateAppointment", date)}
+                        onSelect={ date => handleSelectedDate(date)}
+                        placeholderText="Selecione o dia"
+                        dateFormat="dd/MM/yyyy"
+                        minDate={getDateTomorrow()}
+                        />
+                    );
+                  }}
+                </Field>
               </div>
               <ErrorMessage className="message-error" component="span" name="dateAppointment" />
               <div className="content-input">
                 <p>Qual horário:</p>
                 <Field as="select" name="time" className="input-select">
                   <option value="---">---</option>
-                  <option value="7:00">7:00</option>
-                  <option value="8:00">8:00</option>
-                  <option value="9:00">9:00</option>
+                  {hours.length > 0 && (
+                    hours.map((value, key) => (
+                      <option key={key} value={value}>{value}</option>
+                    ))
+                  )}
                 </Field>
               </div>
               <ErrorMessage className="message-error" component="span" name="time" />
