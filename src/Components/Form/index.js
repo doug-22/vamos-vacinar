1597ad09dfe6a20f128bb9bf10fbf5cfe6d1499c
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-22 14:47:33 -0300

    Refactoring form and navigate tabs

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -31,6 +31,7 @@ export default function Form({handleSubmit, initialValues}) {
   const filterHours = (list) => {
     let listFilter = ["07:00", "08:00", "09:00", "10:00","11:00","12:00","14:00","15:00","16:00","17:00"];
     if(!(list === undefined)){
+      // eslint-disable-next-line
       list.map((hour) => {
         if(listFilter.includes(hour)){
           let count = list.filter(x => x === hour).length;
